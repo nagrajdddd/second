9b23bef1cefdb6637d2707553a811bc7b726eda6
nagrajdddd <nagaraj.dysanur@mccoygloballinks.com>
2017-03-09 11:41:36 +0530

    simply

---
+++ HelloSpring/src/com/mccoy/HelloController.java
@@ -0,0 +1,16 @@
+package com.mccoy;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloController {
+
+	@RequestMapping("/welcome")
+	public ModelAndView helloWorld() {
+
+		ModelAndView model=new ModelAndView("HelloPage");
+		model.addObject("msg","hello");
+		return model;
+	}
+}
